341718c75b8b6007cdabca7b1afa84130319a7d3
Admin <plehno1988@gmail.com>
2024-09-07 09:30:52 +0300

    s xxl gulp html and css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,10 @@ let burger = document.querySelector(".burger")
 burger.onclick = () => {
     let menu = document.querySelector(".header__menu__container")
     let btn = document.querySelector(".header__menu__btn")
+
     menu.classList.toggle("show")
     btn.classList.toggle("show")
+    
     burger.classList.toggle("burger-close")
 
 }
